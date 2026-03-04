1e2d12fa79ef816c84e411ad322f014ea3fd8563
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-03-04 17:51:36 +0100

    feat: register models with admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import (
+    Category,
+    Product,
+    ProductAttribute,
+    ProductImage,
+    ProductVariation,
+)
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(ProductImage)
+admin.site.register(ProductVariation)
+admin.site.register(ProductAttribute)
