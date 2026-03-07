66ec2f3f56280f54b35529394108e63281f0c415
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-03-07 13:16:49 +0100

    feat: checkout urls

---
+++ checkout/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("checkout/", views.checkout, name="checkout"),
+    path("checkout/pay/", views.initialize_payment, name="checkout_pay"),
+    path("checkout/verify/", views.verify_payment, name="checkout_verify"),
+    path("checkout/webhook/", views.paystack_webhook, name="paystack_webhook"),
+    path(
+        "checkout/confirmation/<str:order_number>/",
+        views.order_confirmation,
+        name="order_confirmation",
+    ),
+]
