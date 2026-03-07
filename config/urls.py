a220de6e34a7f6f0855bc06bd1d07f3ac5fa0f6f
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-03-07 14:30:10 +0100

    chore: urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+
+from config import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -7,4 +10,9 @@ urlpatterns = [
     path("shop/", include("products.urls")),
     path("", include("accounts.urls")),
     path("", include("cart.urls")),
+    path("", include("checkout.urls")),
+    path("", include("admin_panel.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
