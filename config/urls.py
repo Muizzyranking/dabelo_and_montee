7888c7892532d39af5060d1343621e6a03f19d40
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-03-09 13:28:55 +0100

    chore: include image serving url and error pages

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
+from django.conf.urls import handler404, handler500
+
 from config import settings
+
+handler404 = "core.views.page_not_found"
+handler500 = "core.views.server_error"
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,6 +18,7 @@ urlpatterns = [
     path("", include("cart.urls")),
     path("", include("checkout.urls")),
     path("", include("admin_panel.urls")),
+    path("images/", include("images.urls")),
 ]
 
 if settings.DEBUG:
