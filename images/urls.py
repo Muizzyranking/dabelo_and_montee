aa28a51af07fd9b22853604202ace87c28d93d0c
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-03-09 13:26:05 +0100

    feat: view to serve image with browser cache

---
+++ images/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("serve/<uuid:image_id>/", views.serve_image, name="image_serve"),
+]
--- images/views.py
+++ images/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from __future__ import annotations
 
-# Create your views here.
+import logging
+
+from django.http import Http404, HttpResponse
+from django.views.decorators.cache import cache_control
+from django.views.decorators.http import condition
+
+from .models import Image, ImageStatus
+from .services import ImageService
+
+logger = logging.getLogger(__name__)
+
+BROWSER_CACHE_MAX_AGE = 60 * 60 * 24 * 7  # 1 week
+
+
+def _etag(request, image_id):
+    try:
+        img = Image.objects.get(pk=image_id)
+        return f'"{img.id}-{img.updated_at.timestamp()}"'
+    except Image.DoesNotExist:
+        return None
+
+
+def _last_modified(request, image_id):
+    try:
+        return Image.objects.get(pk=image_id).updated_at
+    except Image.DoesNotExist:
+        return None
+
+
+@cache_control(public=True, max_age=BROWSER_CACHE_MAX_AGE)
+@condition(etag_func=_etag, last_modified_func=_last_modified)
+def serve_image(request, image_id: str) -> HttpResponse:
+    try:
+        image = Image.objects.get(pk=image_id)
+    except Image.DoesNotExist:
+        raise Http404
+
+    if image.status == ImageStatus.PENDING:
+        return HttpResponse(status=202, headers={"Retry-After": "5"})
+
+    if image.status == ImageStatus.FAILED:
+        raise Http404
+
+    try:
+        data = ImageService.load(image)
+    except FileNotFoundError:
+        raise Http404
+
+    return HttpResponse(data, content_type=image.mime_type)
